f239e2de2c672e65a27c905680bd43586e8d5cf1
Araceae <araceaehuang@gmail.com>
2021-05-18 20:19:31 +0800

    add: solution for problem 609

---
+++ leetcode/0609-Find-Duplicate-File-in-System/main.cpp
@@ -0,0 +1,31 @@
+/*
+* Problem: https://leetcode.com/problems/find-duplicate-file-in-system/
+* My Discussion: 
+*/
+
+class Solution {
+public:
+    vector<vector<string>> findDuplicate(vector<string>& paths) {
+        unordered_map<string, vector<string>> m;
+        for (auto &p : paths) {
+            istringstream ss(p);
+            string dir, str;
+            ss >> dir;
+            while (ss >> str) {
+                string filename, content;
+                for (int i = 0; i < str.size(); ++i) {
+                    if (str[i] == '(') {
+                        filename = str.substr(0, i);
+                        content = str.substr(i+1);
+                        m[content].push_back(dir + '/' + filename);
+                    }
+                }
+            }
+        }
+        vector<vector<string>> res;
+        for (auto it = m.begin(); it != m.end(); ++it)
+            if (it->second.size() > 1)
+                res.push_back(it->second);
+        return res;
+    }
+};
